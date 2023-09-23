69e58bae63f3b0c808ba080680751069140a4513
Edmilton <edmiltonsouzamsc@gmail.com>
2023-09-23 07:19:27 -0300

    Fase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MidCardComponent } from './components/mid-card/mid-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    MidCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
